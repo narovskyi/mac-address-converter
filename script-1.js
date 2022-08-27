044c1a2ada2a0ab555ace9b95630c6003175b396
Oleksii Narovskyi <o.narovskyi@gmail.com>
2022-08-27 18:08:37 +0300

    Done!

---
+++ script-1.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const refs = {
+    inputString: document.querySelector('.input-string'),
+    outputString: document.querySelector('.output-string'),
+    convertButton: document.querySelector('.convert-button')
+};
+
+const convertMac = (event) => {
+    event.preventDefault();
+    const correctSymbols = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+    const symbArr = refs.inputString.value
+    .split('')
+    .map(symbol => symbol.toLowerCase())
+    .filter(value => correctSymbols.includes(value));
+
+    function incorrectMac(array) {
+
+        if (array.length > 12) {
+            refs.outputString.textContent = "Too many symbols";
+        } else if (array.length < 12) {
+            refs.outputString.textContent = "Not enough symbols";
+        } else {
+            refs.outputString.textContent = `${symbArr[0]}${symbArr[1]}:${symbArr[2]}${symbArr[3]}:${symbArr[4]}${symbArr[5]}:${symbArr[6]}${symbArr[7]}:${symbArr[8]}${symbArr[9]}:${symbArr[10]}${symbArr[11]}`;
+        }
+    };
+
+    incorrectMac(symbArr);
+};
+
+const result = refs.convertButton.addEventListener("click", convertMac);
