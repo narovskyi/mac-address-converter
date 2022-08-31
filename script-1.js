79cd92ccc8eca774af6122ceee236f1a00ffed85
Oleksii Narovskyi <o.narovskyi@gmail.com>
2022-08-31 10:27:46 +0300

    add one string for comfort

--- script-1.js
+++ script-1.js
@@ -3,6 +3,7 @@
 const refs = {
     inputString: document.querySelector('.input-string'),
     outputString: document.querySelector('.output-string'),
+    outputEditedString: document.querySelector('.output-edited-string'),
     convertButton: document.querySelector('.convert-button')
 };
 
@@ -18,10 +19,13 @@ const convertMac = (event) => {
 
         if (array.length > 12) {
             refs.outputString.textContent = "Too many symbols";
+            refs.outputEditedString.textContent = "";
         } else if (array.length < 12) {
             refs.outputString.textContent = "Not enough symbols";
+            refs.outputEditedString.textContent = "";
         } else {
             refs.outputString.textContent = `${symbArr[0]}${symbArr[1]}:${symbArr[2]}${symbArr[3]}:${symbArr[4]}${symbArr[5]}:${symbArr[6]}${symbArr[7]}:${symbArr[8]}${symbArr[9]}:${symbArr[10]}${symbArr[11]}`;
+            refs.outputEditedString.textContent = `grep ${symbArr[0]}${symbArr[1]}:${symbArr[2]}${symbArr[3]}:${symbArr[4]}${symbArr[5]}:${symbArr[6]}${symbArr[7]}:${symbArr[8]}${symbArr[9]}:${symbArr[10]}${symbArr[11]} /var/log/dhcpd.log`;
         }
     };
 
